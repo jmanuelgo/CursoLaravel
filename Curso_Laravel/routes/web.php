05019fc8d98c73bd1af244a3f0fae80c1fd78ccc
jmanuelgo <gandamanu330@gmail.com>
2024-09-09 12:32:33 -0400

    Rutas Laravel

--- Curso_Laravel/routes/web.php
+++ Curso_Laravel/routes/web.php
@@ -3,5 +3,35 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return route('curso.id',4);
+    //return view('welcome');
 });
+ /*Route::get ('/contacto',function(){
+    return"Hola desde la pagina de contacto";
+ });
+ 
+ Route::post('/contacto', function () {
+     return"Hola desde post";
+ });*/
+ Route::match(['get','post'],'/contacto',function(){
+    return"Hola desde la pagina GET y POST";
+ });
+
+ Route:: get ('/cursos/Informacion',function(){
+    return"Aqui podrás encontrar toda la información de los cursos";
+ })->name('curso.Informacion');
+
+ Route::get('/cursos/{curso}/{categoria?}',function($curso,$categoria=null){
+
+    if($categoria){
+        return "Bienvenido al curso: $curso de la categoria $categoria";
+    } 
+    else{
+        return"Bienvenido al curso: $curso";
+    }
+ })->whereIn('curso',['php','laravel']);
+ Route::get('/id/{id}',function($id){
+    return"Curson con id: $id";
+ })->name('curso.id');
+
+ 
